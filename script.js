f19423b6fec280e14010e6c2d6b8755e07a6e65f
Michael Poltavskiy <michaelpoltavskiyforcoding@gmail.com>
2026-06-21 15:50:30 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function playNoteA() {
 
     oscillator.type = 'sine'; // форма волны
     oscillator.frequency.value = 440; // Ля первой октавы
-    gainNode.gain.value = 0.15;
+    gainNode.gain.value = 0.2;
 
     oscillator.connect(gainNode);
     gainNode.connect(audioContext.destination);
@@ -82,6 +82,23 @@ counter.addEventListener('input', () => {
 const startBNT = document.getElementById('start-btn');
 const window1 = document.getElementById('window1ForStart');
 const window2 = document.getElementById('window2ForTimer');
+
+//Массив нот
+
+const notes = [
+    { name: "До", file: "music/c.mp3" },
+    { name: "До#", file: "music/cSharp.mp3" },
+    { name: "Ре", file: "music/d.mp3" },
+    { name: "Ми♭", file: "music/eFlat.mp3" },
+    { name: "Ми", file: "music/e.mp3" },
+    { name: "Фа", file: "music/f.mp3" },
+    { name: "Фа#", file: "music/fSharp.mp3" },
+    { name: "Соль", file: "music/g.mp3" },
+    { name: "Соль#", file: "music/gSharp.mp3" },
+    { name: "Ля", file: "music/a.mp3" },
+    { name: "Си♭", file: "music/bFlat.mp3" },
+    { name: "Си", file: "music/b.mp3" }
+];
 
 startBNT.addEventListener('click', () => {
     window1.style.display = 'none';
@@ -123,11 +140,43 @@ startBNT.addEventListener('click', () => {
 
     }
 
+// Рандомная нота
+
     function startRandomNote() {
         svgForStart.src = 'image/ListenLight.svg';
         svgForStart.alt = 'Человек внимательно слушает музыку';
         svgForStart.style.display = 'flex';
         timerP.style.display = 'none';
+        
+        function playRandomNote() {
+            const randomIndex = Math.floor(Math.random() * notes.length);
+
+            console.log(notes[randomIndex].name)
+
+            const audio = new Audio(
+                notes[randomIndex].file
+            );
+
+            const audioContext = new AudioContext();
+            const source = audioContext.createMediaElementSource(audio)
+
+
+            const gainNode = audioContext.createGain();
+            gainNode.gain.value = 4;
+
+            source.connect(gainNode);
+            gainNode.connect(audioContext.destination);
+
+            audio.play()
+
+            setTimeout(() => {
+                audio.pause();
+                audio.currentTime = 0;
+            }, Number(counter.value) * 1000);
+        }
+
+        playRandomNote();
+
     }
 
 })
